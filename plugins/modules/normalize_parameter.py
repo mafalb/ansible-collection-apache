9f4d331b36347d98383488c096c519d8774bb2b0
Markus Falb <markus.falb@mafalb.at>
2021-10-14 02:30:13 +0200

    make lint happy

--- plugins/modules/normalize_parameter.py
+++ plugins/modules/normalize_parameter.py
@@ -5,7 +5,8 @@
 # GNU General Public License v3.0+
 # see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt
 
-# This is a virtual module that is entirely implemented as an action plugin and runs on the controller
+# This is a virtual module that is entirely implemented as an action plugin
+# and runs on the controller
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
